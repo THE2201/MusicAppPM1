16bb5092faaab211b0be9bda0e944229e1230b1f
THE2201 <115427109+THE2201@users.noreply.github.com>
2024-07-02 16:41:27 -0600

    Colores principales (colors.xml) Menu de opciones, titulo

--- app/src/main/java/com/example/musicapppm1/MainActivity.java
+++ app/src/main/java/com/example/musicapppm1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.musicapppm1;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -14,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.perfil_usuario){
+            Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId()==R.id.cambio_contrasena) {
+            Toast.makeText(this, "cambio", Toast.LENGTH_SHORT).show();
+        }else if (item.getItemId()==R.id.cerrar_sesion){
+            Toast.makeText(this, "cerrar", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
